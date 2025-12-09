ba894e2aa9683c3ad737356e3bbf6a0b17e5a5b1
Samyra Torres <samyratorres476@gmail.com>
2025-12-09 17:17:24 -0300

    Atividade do ZOO iniciada, 90% feita

---
+++ tasks/poo/zoo/py/draft.py
@@ -0,0 +1,59 @@
+from typing import Any
+from abc import ABC, abstractmethod
+
+class Animal (ABC):
+    def __init__ (self, nome: str):
+        self.nome = nome 
+
+    @abstractmethod
+    def fazer_som(self):
+        pass
+
+    @abstractmethod
+    def mover(self):
+        pass
+
+    def apresentar_nome (self):
+        print (f"Eu sou um(a) {self.nome}")
+
+class Gato (Animal):
+    def __init__ (self, nome: str):
+        super().__init__(nome)
+
+    def fazer_som (self):
+        print ("MIAUUU")
+    
+    def mover (self):
+        print ("Gatinho andou e arranhou o sofá")
+
+class Cachorro (Animal):
+    def __init__ (self, nome:str):
+        super().__init__(nome)
+    
+    def fazer_som (self):
+        print ("AUAUAUUU")
+
+    def mover (self):
+        print ("O doguinho rolou no chão")
+
+class Vaca (Animal):
+    def __init__ (self, nome:str):
+        super().__init__(nome)
+    
+    def fazer_som (self):
+        print ("MUUUUUU")
+    
+    def mover (self):
+        print ("Vaquinha saiu do lugar para ir comer grama")
+
+def apresentar (animal:Animal):
+    animal.apresentar_nome()
+    animal.fazer_som()
+    animal.mover()    
+    print (f"Tipo: {type(animal).__name__}")
+    print("-" * 10)
+
+animais: list[Animal] = [Gato("Gatinho"), Cachorro("Doguinho"), Vaca("Vaquinha")]
+
+for animal in animais:
+    apresentar(animal)
